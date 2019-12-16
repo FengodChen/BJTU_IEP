28f985a151890d076f2a367c7fc67f564a2963c5
FengodChen <a1176553293@outlook.com>
2019-12-17 05:08:13 +0800

    fix apache startup

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
 # [Config]
-export SERVER_NAME='fengodchen_apache_server'
-export SERVER_VERSION='v1.0'
-export SERVER_PORT=10
+export SERVER_NAME='fengodchen_main'
+export DOCKER_NAME='fengodchen_apache_server'
+export DOCKER_VERSION='v1.0'
+export DOCKER_PORT=10
 
 export SMR_PATH=$(pwd)
 
 # [Shell]
 
 # Install Server
-docker build -t ${SERVER_NAME}:${SERVER_VERSION} App/Server/docker/ --rm \
-    && docker run -it -p ${SERVER_PORT}:80 -v ${SMR_PATH}/Share:/Share ${SERVER_NAME}:${SERVER_VERSION}
+docker build -t ${DOCKER_NAME}:${DOCKER_VERSION} App/Server/docker/ --rm \
+    && docker run -itd -p ${DOCKER_PORT}:80 -v ${SMR_PATH}/Share:/Share --name ${SERVER_NAME} ${DOCKER_NAME}:${DOCKER_VERSION} \
+    && docker start ${SERVER_NAME}
