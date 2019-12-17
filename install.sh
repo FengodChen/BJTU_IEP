64a3782ca84aae7983ba7b4d623c0d699b9db370
FengodChen <a1176553293@outlook.com>
2019-12-17 15:40:48 +0800

    fix time zone, add php

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 
 # [Config]
-export SERVER_NAME='fengodchen_main'
-export DOCKER_NAME='fengodchen_apache_server'
-export DOCKER_VERSION='v1.0'
-export DOCKER_PORT=10
-
-export SMR_PATH=$(pwd)
+source config
 
 # [Shell]
 
 # Install Server
 docker build -t ${DOCKER_NAME}:${DOCKER_VERSION} App/Server/docker/ --rm \
-    && docker run -itd -p ${DOCKER_PORT}:80 -v ${SMR_PATH}/Share:/Share --name ${SERVER_NAME} ${DOCKER_NAME}:${DOCKER_VERSION} \
+    && docker run -itd -p ${DOCKER_PORT}:80 -v ${SMR_PATH}/Share:/Share -v ${SMR_PATH}/App/Server/html:/var/www/html --name ${SERVER_NAME} ${DOCKER_NAME}:${DOCKER_VERSION} \
     && docker start ${SERVER_NAME}
