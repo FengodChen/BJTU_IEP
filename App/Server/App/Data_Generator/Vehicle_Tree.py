886b10a4abc09855358da9afddc1cb3349e2a5ff
FengodChen <a1176553293@outlook.com>
2020-01-20 13:13:34 +0800

    add Vehicle_Tree.py

---
+++ App/Server/App/Data_Generator/Vehicle_Tree.py
@@ -0,0 +1,63 @@
+import time
+import hashlib
+import sqlite3
+import os
+
+def str2sha256(s):
+    '''
+    str2sha256(str) -> str
+    '''
+    sha = hashlib.sha256()
+    sha.update(s.encode('utf-8'))
+    return sha.hexdigest()
+
+def getDate():
+    '''
+    getDate(void) -> "YYYY-MM-DD"
+    '''
+    string_time = time.strftime("%Y-%m-%d", time.localtime())
+    return string_time
+
+class TreeDB:
+    def __init__(self, treePath, db_name):
+        self.treePath = treePath
+        self.db_name = db_name
+        self.db = sqlite3.connect("{}/{}".format(treePath, db_name))
+    
+    def findData(self, roadName, date):
+        '''
+        TreeDB.findData(str, str) -> str
+
+        Return paired db filePath. If fair, return "0"
+        '''
+
+        try:
+            filename = self.db.execute("SELECT FILENAME FROM {} WHERE DATE IS \"{}\";".format(roadName, date)).fetchone()[0]
+            return "{}/{}/index.db".format(self.treePath, filename)
+        except sqlite3.OperationalError:
+            return "0"
+    
+    def insertData(self, roadName, date):
+        hash_data = str2sha256("{}{}".format(roadName, date))
+        if (not self.hasTable(roadName)):
+            self.createTable(roadName)
+        self.db.execute("INSERT INTO {} VALUES (\"{}\", \"{}\");".format(roadName, date, hash_data))
+        self.db.commit()
+        os.makedirs("{}/{}".format(self.treePath, hash_data))
+
+    def createTable(self, roadName):
+        '''
+        TreeDB.createTable(str)
+        '''
+        self.db.execute("CREATE TABLE {}(DATE TEXT NOT NULL, FILENAME TEXT NOT NULL);".format(roadName))
+        self.db.commit()
+    
+    def hasTable(self, roadName):
+        '''
+        Judge if the table exist
+        '''
+        try:
+            self.db.execute("SELECT * FROM {};".format(roadName))
+            return True
+        except:
+            return False
